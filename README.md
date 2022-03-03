53ab7fa75b4f760ea6d1944dd64f22955d143f09
Suraj Das <suraj.trapical@gmail.com>
2022-03-03 18:13:18 +0530

    1.0.3 fixed nav bar on mobile devices

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - [x] descriptions
 - [x] all pages available
 - [ ] The recent pages component on the index page should show only a few posts
+- [x] Fix the nav on mobile screens
--- src/components/nav.js
+++ src/components/nav.js
@@ -5,9 +5,9 @@ function Nav() {
   return (
     <nav className='mt-6 mb-8'>
         <ul className='flex pl-0'>
-          <li className='pr-8 list-none'><Link to="/" className='text-slate-700'>Home</Link></li>
-          <li className='pr-8 list-none'><Link to="/dummy" className='text-slate-700'>About Me</Link></li>
-          <li className='pr-8 list-none'><Link to="/all-pages" className='text-slate-700'>All Pages</Link></li>
+          <li className='pr-8 list-none'><Link to="/" className='text-sm md:text-base text-slate-700'>Home</Link></li>
+          <li className='pr-8 list-none'><Link to="/dummy" className='text-sm md:text-base text-slate-700'>About Me</Link></li>
+          <li className='pr-8 list-none'><Link to="/all-pages" className='text-sm md:text-base text-slate-700'>All Pages</Link></li>
         </ul>
     </nav>
   )
